1f738bf7b8e0175a7cf6e0ae9602d72fd74615de
FiammaMuscari <fiammamuscari@gmail.com>
2023-07-25 19:21:35 -0300

    fix

--- pages/components/Grid.jsx
+++ pages/components/Grid.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import Card from "./Card";
 
 const Grid = ({ properties }) => {
+  if (!properties || properties.length === 0) {
+    return <p>No properties available</p>;
+  }
+
   return (
     <div className="grid m-[5em] grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {properties.map((property) => (
